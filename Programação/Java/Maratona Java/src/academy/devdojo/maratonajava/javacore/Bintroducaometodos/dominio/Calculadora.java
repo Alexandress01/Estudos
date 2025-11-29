54a6cec1533dc3518c035f642380ee71bce8337e
Alexandre Silva Santos <alesantoscom06@gmail.com>
2025-11-29 16:17:47 -0300

    estudos: classes e objetos; métodos

---
+++ Programação/Java/Maratona Java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+    public void somaDois() {
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDois() {
+        System.out.println(21 - 2);
+    }
+
+    public void multiplicaDois(int num1, int num2) {
+        System.out.println(num1 * num2);
+    }
+}
---
+++ Programação/Java/Maratona Java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.multiplicaDois(10, 20);
+
+        System.out.println("Finalizando CalculadoraTest02");
+    }
+}
