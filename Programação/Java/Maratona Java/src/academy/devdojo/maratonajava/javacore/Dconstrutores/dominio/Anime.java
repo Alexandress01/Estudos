100d294ec923fcfff425959b1268609b4e739ae2
Alexandre Silva Santos <alesantoscom06@gmail.com>
2025-12-04 20:23:43 -0300

    estudos: orientação a objetos; construtores

---
+++ Programação/Java/Maratona Java/src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -0,0 +1,59 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.dominio;
+
+public class Anime {
+    private String nome;
+    private String genero;
+    private int episodios;
+    private String estudio;
+
+    public Anime(String nome, String genero, int episodios, String estudio) {
+        System.out.println("Dentro do construtor");
+        this.nome = nome;
+        this.genero = genero;
+        this.episodios = episodios;
+        this.estudio = estudio;
+    }
+
+    public Anime() {
+
+    }
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.genero);
+        System.out.println(this.episodios);
+        System.out.println(this.estudio);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+
+    public int getEpisodios() {
+        return this.episodios;
+    }
+
+    public void setEstudio(String estudio) {
+        this.estudio = estudio;
+    }
+
+    public String getEstudio() {
+        return estudio;
+    }
+}
