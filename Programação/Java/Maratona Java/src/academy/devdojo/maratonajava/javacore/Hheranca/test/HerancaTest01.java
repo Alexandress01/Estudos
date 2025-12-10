c2c867287f524bc188943b75a468d499e5c4fd37
Alexandre Silva Santos <alesantoscom06@gmail.com>
2025-12-09 21:22:15 -0300

    estudos: orientação a objetos; herança

---
+++ Programação/Java/Maratona Java/src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+
+        endereco.setRua("Rua Jardim das Laranjeiras");
+        endereco.setCep("40028-922");
+
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.setNome("Aleki");
+        pessoa.setCpf("012.345.678-00");
+        pessoa.setEndereco(endereco);
+
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+
+        funcionario.setNome("Carlão Caminhoneiro");
+        funcionario.setCpf("00.876.543-21");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(7550.33);
+
+        System.out.println("---------------------");
+
+        funcionario.imprime();
+    }
+}
