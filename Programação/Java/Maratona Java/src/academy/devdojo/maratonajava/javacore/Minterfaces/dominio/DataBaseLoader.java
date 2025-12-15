6844442d4c2e08024b1c4ff3a9784c853b3cbf45
Alexandre Silva Santos <alesantoscom06@gmail.com>
2025-12-15 16:24:45 -0300

    estudos: interfaces; introdução, métodos e atributos

---
+++ Programação/Java/Maratona Java/src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataBaseLoader.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
+
+public class DataBaseLoader implements DataLoader, DataRemover {
+    // private -> default -> protected -> public
+    @Override
+    public void load() {
+        System.out.println("Carregando dados do banco...");
+    }
+
+    @Override
+    public void remove() {
+        System.out.println("Removendo dados do banco...");
+    }
+
+    @Override
+    public void checkPermission() {
+        System.out.println("Checando permissões no banco...");
+    }
+
+    public static void retrieveMaxDataSize() {
+        System.out.println("Dentro do retrieveMaxDataSize na classe DataBaseLoader");
+    }
+}
