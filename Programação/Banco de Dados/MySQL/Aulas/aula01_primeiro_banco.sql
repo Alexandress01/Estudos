5820654d7a2b43b7bf10761fb4cc127dcea1ada7
Alexandre Silva Santos <alesantoscom06@gmail.com>
2025-12-15 15:32:25 -0300

    estudos: primeiro banco de dados

---
+++ Programação/Banco de Dados/MySQL/Aulas/aula01_primeiro_banco.sql
@@ -0,0 +1,18 @@
+-- Aula 01: Criando o primeiro banco de dados
+-- Aprendi:
+-- - Como criar um banco de dados
+-- - Como selecionar um banco com USE
+-- - Como criar tabelas e campos
+-- - Quais são os tipos primitivos
+
+CREATE DATABASE cadastro;
+USE cadastro;
+
+CREATE TABLE pessoas (
+    nome VARCHAR(100),
+    idade INT(3),
+    sexo CHAR(1),
+    altura FLOAT,
+    peso FLOAT,
+    nacionalidade VARCHAR(30)
+);
