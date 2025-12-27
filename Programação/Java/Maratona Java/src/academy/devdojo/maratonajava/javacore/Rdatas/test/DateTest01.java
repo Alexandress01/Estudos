0d3702973f6c8c22e0a88060c4746ef97cdd84de
Alexandre Silva Santos <alesantoscom06@gmail.com>
2025-12-27 15:57:08 -0300

    estudos: datas

---
+++ Programação/Java/Maratona Java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/DateTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1766860285626L); // long 100000
+
+        date.setTime(date.getTime() + 3_600_000);
+
+        System.out.println(date);
+    }
+}
