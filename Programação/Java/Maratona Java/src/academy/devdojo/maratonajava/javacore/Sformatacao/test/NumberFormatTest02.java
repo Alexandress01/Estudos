3f0ff270a4363a3c0fb173384b0db093b6b563d0
Alexandre Silva Santos <alesantoscom06@gmail.com>
2025-12-28 16:22:30 -0300

    estudos: formatação

---
+++ Programação/Java/Maratona Java/src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
@@ -0,0 +1,35 @@
+package academy.devdojo.maratonajava.javacore.Sformatacao.test;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class NumberFormatTest02 {
+    public static void main(String[] args) {
+        Locale localeDefault = Locale.getDefault();
+        Locale localeBR = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+
+        NumberFormat[] numberFormats = new NumberFormat[4];
+
+        numberFormats[0] = NumberFormat.getCurrencyInstance();
+        numberFormats[1] = NumberFormat.getCurrencyInstance(localeJP);
+        numberFormats[2] = NumberFormat.getCurrencyInstance(localeBR);
+        numberFormats[3] = NumberFormat.getCurrencyInstance(localeIT);
+
+        double valor = 1000.2130;
+
+        for (NumberFormat numberFormat : numberFormats) {
+            System.out.println(numberFormat.getMaximumFractionDigits());
+            System.out.println(numberFormat.format(valor));
+        }
+        String valorString = "￥1,000";
+
+        try {
+            System.out.println(numberFormats[1].parse(valorString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
