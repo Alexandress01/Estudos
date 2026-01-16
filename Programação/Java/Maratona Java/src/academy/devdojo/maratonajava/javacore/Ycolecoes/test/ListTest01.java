d52ba473b0731d63fba53ea54b557b9c71fc6a9f
Alexandre Silva Santos <alesantoscom06@gmail.com>
2026-01-16 18:58:55 -0300

    estudos: coleções; list

--- Programação/Java/Maratona Java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ Programação/Java/Maratona Java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -6,9 +6,17 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(16); // 1.5
+        List<String> nomes2 = new ArrayList<>(16);
 
         nomes.add("Alexandre");
-        nomes.add("DevDojo Academy");
+        nomes.add("DevDojo");
+
+        nomes2.add("Santos");
+        nomes2.add("Academy");
+
+//        nomes.remove("Alexandre");
+
+        nomes.addAll(nomes2);
 
         for (String nome : nomes) {
             System.out.println(nome);
@@ -21,5 +29,27 @@ public class ListTest01 {
         for (int i = 0; i < nomes.size(); i++) {
             System.out.println(nomes.get(i));
         }
+
+        System.out.println(nomes);
+
+        System.out.println("--------------------");
+
+        List<Integer> numeros = new ArrayList<>(16);
+
+        numeros.add(1);
+        numeros.add(15);
+        numeros.add(7);
+
+        for (Integer numero : numeros) {
+            System.out.println(numero);
+        }
+
+        System.out.println("--------------------");
+
+        for (int i = 0; i < numeros.size(); i++) {
+            System.out.println(numeros.get(i));
+        }
+
+        System.out.println(numeros);
     }
 }
