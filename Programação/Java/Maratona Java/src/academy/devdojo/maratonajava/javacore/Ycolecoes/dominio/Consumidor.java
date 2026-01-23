1145dd8e98f2940c6cfd30db15d6dac82d76ae94
Alexandre Silva Santos <alesantoscom06@gmail.com>
2026-01-23 16:24:23 -0300

    estudos: coleções; map, hashMap e linkedHashMap

---
+++ Programação/Java/Maratona Java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Consumidor.java
@@ -0,0 +1,42 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.dominio;
+
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Consumidor {
+    private long id;
+    private String nome;
+
+    public Consumidor(String nome) {
+        this.id = ThreadLocalRandom.current().nextLong(0, 100_000);
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Consumidor{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Consumidor that = (Consumidor) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
