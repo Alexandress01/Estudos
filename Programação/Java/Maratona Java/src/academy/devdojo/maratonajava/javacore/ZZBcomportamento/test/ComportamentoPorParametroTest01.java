708b6014261bc1aa7c117876eddd05926132b5a0
Alexandre Silva Santos <alesantoscom06@gmail.com>
2026-02-02 17:12:15 -0300

    estudos: parametrizando comportamentos

---
+++ Programação/Java/Maratona Java/src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/test/ComportamentoPorParametroTest01.java
@@ -0,0 +1,42 @@
+package academy.devdojo.maratonajava.javacore.ZZBcomportamento.test;
+
+import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComportamentoPorParametroTest01 {
+    private static List<Car> cars = List.of(new Car("green", 2011), new Car("black", 1998), new Car("red", 2019));
+
+    public static void main(String[] args) {
+        System.out.println(filterCarByColor(cars, "green"));
+        System.out.println(filterCarByColor(cars, "red"));
+
+        System.out.println("-------------------------");
+
+        System.out.println(filterCarByYearBefore(cars, 2020));
+        System.out.println(filterCarByYearBefore(cars, 2012));
+    }
+
+    private static List<Car> filterCarByColor(List<Car> cars, String color) {
+        List<Car> filteredCar = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getColor().equals(color)) {
+                filteredCar.add(car);
+            }
+        }
+        return filteredCar;
+    }
+
+    private static List<Car> filterCarByYearBefore(List<Car> cars, int year) {
+        List<Car> filteredCar = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getYear() < year) {
+                filteredCar.add(car);
+            }
+        }
+        return filteredCar;
+    }
+}
